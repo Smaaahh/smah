bcfff5531014263b80c37c7503eb698f07124ed2
Unknown <helene.chaumette@gmail.com>
2017-10-13 14:24:49 +0200

    test car.cs

---
+++ smaaahh-dao/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smaaahh_dao
+{
+    class Car
+    {
+        [Key]
+        public int CarId { get ; set ; }
+        public int PlaceNumber { get; set; }
+        public string Model { get; set; }
+
+        public string CarPlate { get; set; }
+
+        public int DriverId { get; set; }
+        [ForeignKey("DriverId")]
+        public Driver Driver { get; set; }
+
+    }
+}
