b0c180f16b1f4a1a05366b69caca00ccc24a45fd
Unknown <helene.chaumette@gmail.com>
2017-10-17 16:40:32 +0200

    Modif interface admin

---
+++ smaaahh-wpf/Classes/Functions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace smaaahh_wpf
+{
+    public  class Functions
+    {
+        protected static string ApiUrl = "http://localhost:51453/";
+
+        public async static Task<T> CallApi<T>(string url, bool needAuth)
+        {
+            HttpClient user = new HttpClient();
+            user.BaseAddress = new Uri(ApiUrl);
+            user.DefaultRequestHeaders.Accept.Clear();
+            T s = default(T);
+            user.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            HttpResponseMessage response = await user.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                s = await response.Content.ReadAsAsync<T>();
+            }
+            return s;
+        }
+
+    }
+}
